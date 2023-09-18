d74e04ed676d254caebf54e189a3f88076c6f40c
felpe46 <fl205286@gmail.com>
2023-09-18 11:05:16 -0300

    fim

---
+++ src/ls1ex05.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ls1ex05 {
+    public static void main(String[] args){
+        double Sm;
+        Sm = 788;
+        double SmUsuario;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite quanto você ganha mensalmente: ");
+        SmUsuario = sc.nextDouble();
+
+        System.out.printf("Você ganha o equivalente a %.2f salarios minimos.", SmUsuario / Sm);
+    }
+}
