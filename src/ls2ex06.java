39d0eed201c28caeb69f26140864f74403749d1f
felpe46 <130997511+felpe46@users.noreply.github.com>
2023-10-26 19:45:19 -0300

    Add files via upload

---
+++ src/ls2ex06.java
@@ -0,0 +1,27 @@
+//Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos
+//são VERDADEIROS ou FALSOS.
+
+import java.util.Random;
+
+public class ls2ex06 {
+    public static void main(String[] args) {
+
+        Random gerador =  new Random();
+
+        int A = gerador.nextInt(-10, 10);
+        int B = gerador.nextInt(-10, 10);
+        boolean num;
+        int C;
+
+        if (A + B < 0){
+            num = false;
+             C = A + B;
+        }
+        else {
+            num = true;
+            C = A + B;
+        }
+        System.out.println(num);
+        System.out.println(C);
+    }
+}
